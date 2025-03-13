3ea2c9f636eb8ee3b5f8a76dba47bf94346b5a2c
Muskan-kagzi <muskankagzi65@gmail.com>
2025-03-13 19:10:11 +0530

    Create day12.java

---
+++ day12.java
@@ -0,0 +1,101 @@
+// Question_1 :
+// https://leetcode.com/problems/binary-search/description/
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int low=0;
+        int high=nums.length-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(target>nums[mid]){
+                low=mid+1;
+            }
+            else if(target==nums[mid]){
+                return mid;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return -1;
+    }
+}
+TC-O(n)
+SC-O(1)
+Approach-binary search
+
+// Question_2 :
+// https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int first = findFirst(nums, target);
+        int last = findLast(nums, target);
+        return new int[]{first, last};
+    }
+
+    private int findFirst(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int first = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                if (mid == 0 || nums[mid - 1] != target) {
+                    first = mid;
+                    break;
+                } else {
+                    right = mid - 1;
+                }
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return first;
+    }
+
+    private int findLast(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int last = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                if (mid == nums.length - 1 || nums[mid + 1] != target) {
+                    last = mid;
+                    break;
+                } else {
+                    left = mid + 1;
+                }
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return last;
+    }
+}
+TC-O(logn)
+SC-O(n)
+Approach-binary search
+  
+// Question_3 :
+// https://leetcode.com/problems/search-insert-position/description/ 
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length-1;
+
+        while (start <= end) {
+            int mid = start + (end-start)/2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] > target) end = mid-1;
+            else start = mid+1;
+        }
+
+        return start;
+    }
+}
+TC-O(logn)
+SC-O(1)
+Approach-binary search
